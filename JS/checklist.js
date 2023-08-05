b2e1cd12bf1584884ee310c1898e488c18979788
Maria Eduarda Bernardo <mariaedu9434@gmail.com>
2023-08-05 14:39:17 -0300

    Alteracao no codigo do checklist

--- JS/checklist.js
+++ JS/checklist.js
@@ -1,33 +1,33 @@
-const ongs = JSON.parse(relatedONGs) [
-    {
+const ongs = [
+  {
       nome: "A gente ajuda",
-      itens: ["Financeira", "Roupa","Alimento","Voluntario","Campanha"]
-    },
-    {
+      itens: ["Financeira", "Roupa", "Alimento", "Voluntario", "Campanha"]
+  },
+  {
       nome: "Ação da cidadania",
-      itens: ["Financeira", "Roupa","Brinquedo","Alimento","Voluntario","Cobertor","Campanha","Animais","Higiene"]
-    },
-    {
+      itens: ["Financeira", "Roupa", "Brinquedo", "Alimento", "Voluntario", "Cobertor", "Campanha", "Animais", "Higiene"]
+  },
+  {
       nome: "Caminha Down",
-      itens: ["Financeira","Voluntario","Campanha"]
-    },
-    {
+      itens: ["Financeira", "Voluntario", "Campanha"]
+  },
+  {
       nome: "Escoteiros do Brasil",
-      itens: ["Financeira","Voluntario","Campanha"]
-    },
-    {
+      itens: ["Financeira", "Voluntario", "Campanha"]
+  },
+  {
       nome: "Hamburgada do bem",
       itens: ["Financeira", "Voluntario"]
-    },
-    {
+  },
+  {
       nome: "Instituto Mara Gabrilli",
-      itens: ["Financeira","Voluntario","Campanha"]
-    },
-    {
+      itens: ["Financeira", "Voluntario", "Campanha"]
+  },
+  {
       nome: "Themis Furigo",
       itens: ["Financeira"]
-    }
-  ];
+  }
+];
 
   const ongURLs = {
     "A gente ajuda": "ong_1.html",
@@ -47,8 +47,6 @@ const ongs = JSON.parse(relatedONGs) [
       return selectedOptions.every(option => ong.itens.includes(option));
     });
 
-
-
     // Constrói a URL de redirecionamento com os parâmetros de consulta
     const url = new URL('resultado.html', window.location.href);
     url.searchParams.append('ongs', JSON.stringify(relatedONGs));
@@ -57,33 +55,33 @@ const ongs = JSON.parse(relatedONGs) [
     window.location.href = url.href;
   }
 
-
 // Obtém as ONGs relacionadas da URL de consulta
 const urlParams = new URLSearchParams(window.location.search);
 const relatedONGs = urlParams.get('ongs');
 
-const ongContainer = document.getElementById("ong-container");
-
-// Exibe as ONGs nos cards
 const ongList = document.getElementById('ongList');
-ongs.forEach(ong => {
-    const card = document.createElement('div');
-    card.classList.add('ong-card');
+if (relatedONGs) {
+    const ongsToShow = JSON.parse(relatedONGs);
 
-    const title = document.createElement('h6');
-    const link = document.createElement('a');
-    link.href = getONGURL(ong.nome);
-    link.textContent = ong.nome;
-    title.appendChild(link);
+    ongsToShow.forEach(ong => {
+        const card = document.createElement('div');
+        card.classList.add('ong-card');
 
-    const description = document.createElement('p');
-    description.textContent = ong.descricao;
+        const title = document.createElement('h6');
+        const link = document.createElement('a');
+        link.href = getONGURL(ong.nome);
+        link.textContent = ong.nome;
+        title.appendChild(link);
 
-    card.appendChild(title);
-    card.appendChild(description);
+        const description = document.createElement('p');
+        description.textContent = ong.descricao;
 
-    ongList.appendChild(card);
-});
+        card.appendChild(title);
+        card.appendChild(description);
+
+        ongList.appendChild(card);
+    });
+}
 
 function getONGURL(nome) {
     return ongURLs[nome];
